403596cce770b463dba4d69f90e8a9870c0b6d02
m2en <merunno92@gmail.com>
2022-07-10 01:41:38 +0900

    chore: add global css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 function main() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1 className={'text-3xl font-bold underline'}>Hello World</h1>
+    </div>
+  );
 }
 
 export default main;

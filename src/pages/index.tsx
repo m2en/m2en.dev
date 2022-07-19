a13fbd823a3fc5b42d390930a62f7e3ea7fdcede
m2en <merunno92@gmail.com>
2022-07-20 02:41:45 +0900

    feat: 真ん中に配置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,31 @@
 import { ThemeButton } from '../components/theme-button';
 import React from 'react';
-import { Container } from '@chakra-ui/react';
+import { Box, Container, VStack, Wrap } from '@chakra-ui/react';
 import Head from 'next/head';
 
 function main() {
   return (
-    <Container>
-      <Head>
-        <title>m2en</title>
-        <meta name={'description'} content={"m2en's portfolio"} />
-        <meta name={'og:title'} content={"m2en's portfolio"} />
-        <meta name={'og:description'} content={"m2en's portfolio"} />
-        <meta name={'og:url'} content={'https://m2en.dev'} />
-        <meta name={'og:site_name'} content={'m2en'} />
-        <meta name={'og:type'} content={'website'} />
-        <meta name={'twitter:card'} content={'summary'} />
-        <meta name={'twitter:site'} content={'@m2en'} />
-      </Head>
-      <ThemeButton />
-    </Container>
+    <VStack>
+      <Container maxW={'md'}>
+        <Head>
+          <title>m2en</title>
+          <meta name={'description'} content={"m2en's portfolio"} />
+          <meta name={'og:title'} content={"m2en's portfolio"} />
+          <meta name={'og:description'} content={"m2en's portfolio"} />
+          <meta name={'og:url'} content={'https://m2en.dev'} />
+          <meta name={'og:site_name'} content={'m2en'} />
+          <meta name={'og:type'} content={'website'} />
+          <meta name={'twitter:card'} content={'summary'} />
+          <meta name={'twitter:site'} content={'@m2en'} />
+        </Head>
+      </Container>
+
+      <Wrap spacing={8} shouldWrapChildren={false}>
+        <Box w={'100%'} p={4}>
+          <h1>Hello world</h1>
+        </Box>
+      </Wrap>
+    </VStack>
   );
 }
 
